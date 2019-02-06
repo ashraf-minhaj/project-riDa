852a5dfcbf2ca5c2fb63d75feaf93ad5aea10bd6
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2019-02-07 03:07:46 +0600

    Add files via upload
    This code is to store the data to sheet via WiFi.

---
+++ project_RiDa_test_COde.ino
@@ -0,0 +1,95 @@
+/*
+ * Project rida (river data)
+ * by Ashraf Minhaj 2018
+ * ashraf_minhaj@yahoo.com
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+//define the parameters for the IFTTT
+#define HOST "maker.ifttt.com"
+
+//float temp; 
+
+const char* ssid = "Xvfd";
+const char* password = "brimbrimbrah";
+
+//int led = ;
+int temS = 36;
+float sval = 0;
+float v2 = 302;
+
+WiFiClientSecure client;
+
+void setup() 
+{
+  //pinMode(led,OUTPUT);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Waiting to connect to WiFi... ");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(1000);
+
+
+  
+}
+
+
+void loop() 
+{
+  sval = analogRead(temS);
+  //sval = (analogRead(A0) * 330.0) / 1023.0;
+/*  if (client.connected())
+  {
+    client.stop();
+  }
+
+  client.flush();
+  */
+  if (client.connect(HOST,443)) 
+  {
+    //digitalWrite(led,HIGH);
+    //delay(100);
+    Serial.println("Sending Data to sheet");
+                                       // build the HTTP request
+    String toSend = "GET /trigger/";
+    toSend += "The Jamuna";
+    toSend += "/with/key/";
+    toSend += "RzbSSqTBl3khy4SzoBA0OSXynSqIBQEVfKrM2GuwSX";
+    toSend += "?value1=";
+    toSend += "Temp:+=+";
+    toSend += sval;
+    toSend += "+Turbidity+=+";
+    toSend += v2;
+    toSend += "+pH+=+";
+    toSend += "7";
+    toSend += " HTTP/1.1\r\n";
+    toSend += "Host: ";
+    toSend += HOST;
+    toSend += "\r\n";
+    toSend += "Connection: close\r\n\r\n";
+    client.print(toSend);
+
+  client.flush();
+  client.stop();
+  }
+  /*delay(1000);
+  client.flush();
+  client.stop();  
+  //digitalWrite(led,LOW); 
+  */
+  delay(5000);
+  
+  
+
+}
+
+
