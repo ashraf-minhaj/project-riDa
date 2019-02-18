1e6519497433688b642957e30d6cf30221d413ef
Ashrafur Rahman Minhaj <ashraf_minhaj@yahoo.com>
2019-02-18 11:54:57 +0600

    Update secrets.h

--- rida-x/secrets.h
+++ rida-x/secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "Xvfd"    // replace MySSID with your WiFi network name
-#define SECRET_PASS "brimbrimbrah"  // replace MyPassword with your WiFi password
+#define SECRET_SSID "xxx"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "xyzxyzxx"  // replace MyPassword with your WiFi password
 
 #define SECRET_CH_ID 666892     // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "7O1XQY1FPRSJPVYQ"   // replace XYZ with your channel write API Key
+#define SECRET_WRITE_APIKEY "7O1XQY1FPRSJddds"   // replace XYZ with your channel write API Key
